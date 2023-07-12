644a6ec7dadf22e916522b244df86f19a6d81223
Margaret <maenunezt@gmail.com>
2023-07-12 13:24:22 -0400

    agregando ViewHolder

---
+++ app/src/main/java/com/example/e6m5/BookAdapter.java
@@ -0,0 +1,31 @@
+package com.example.e6m5;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BookAdapter extends RecyclerView.Adapter <BookAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public BookAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
